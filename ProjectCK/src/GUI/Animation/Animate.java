473377f59102d5be1d53406c2ee6f6ec3157f00a
NISHIDO-LAPTOP\admin <quan5555qt@gmail.com>
2021-06-01 13:18:53 +0700

    set all node values to inf when start run

--- ProjectCK/src/GUI/Animation/Animate.java
+++ ProjectCK/src/GUI/Animation/Animate.java
@@ -32,6 +32,7 @@ public class Animate {
     private static DoubleProperty speed = null;
 
     public static void playAnimation(GraphPanel graphview, ResetButton resetBtn, PauseButton pauseButton, ContinueButton continueButton, StopButton stopButton, Animation animation, List<Node> result) {
+        graphview.ReadyToSimulate();
         playAnimation(graphview, resetBtn, pauseButton, continueButton, stopButton, animation, result != null);
     }
 
